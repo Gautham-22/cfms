dce6c76492e9dacc6d08ba280af81207abf557c7
pmihsan <ihsanpmh@gmail.com>
2022-05-24 09:55:01 +0530

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import LoginPage from './components/form';
 function App() {
   return (
     <div className="App">
+      <h1>Hello World</h1>
+      <LoginPage></LoginPage>
       <LoginPage></LoginPage>
     </div>
   );
