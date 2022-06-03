b15e429dc097ddb9baf9e6bcd11240ff36c35f00
praveen-221 <marveen223@gmail.com>
2022-06-03 14:04:58 +0530

    added Admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //pages
 import Homepage from './pages/Homepage/Homepage';
 import Contact from './pages/Contact/Contact';
 import AboutUs from './pages/About Us/Aboutus';
+import Admin from './pages/Admin/Home';
 // import Error from './pages/Errorpage/Errorpage';
 
 import { BiMenuAltRight } from 'react-icons/bi';
@@ -26,9 +28,14 @@ function App() {
           <a href='#contact'>Contact</a>
         </ul>
       </div>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/admin' exact element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Homepage />
       <AboutUs />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
