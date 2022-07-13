be358c96ec34993237491007cbc9ffbe53d5a3db
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2022-07-13 17:08:02 +0530

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
+<<<<<<< Updated upstream
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+=======
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Landing from './Landing';
+import Login from './components/loginSignupPage/login';
+import SignUp from './components/loginSignupPage/signup';
+import Main from './components/dashboard/Main';
+import Account from './components/account/Account';
+import Navbar from './components/navbar/Navbar';
+import Errorpage from './pages/Errorpage/Errorpage';
+import Adminlogin from './components/admin/Adminlogin';
+import Adminhome from './components/admin/Adminhome';
+>>>>>>> Stashed changes
 
 import Login from "./components/loginSignupPage/login";
 import SignUp from "./components/loginSignupPage/signup";
